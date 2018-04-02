c0c226c69ab20ab0e907b133c686095ba10f3cba
zz21y <1570184051@qq.com>
2018-04-02 23:26:40 +0800

    first commit

---
+++ 1.计算器/main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+type Object interface{}
+
+// 运算法则
+var OpMap = map[string]Object{
+	"+": func(v []Object) Object {
+		return v[0].(float64) + v[1].(float64)
+	},
+	"-": func(v []Object) Object {
+		return v[0].(float64) - v[1].(float64)
+	},
+	"*": func(v []Object) Object {
+		return v[0].(float64) * v[1].(float64)
+	},
+	"/": func(v []Object) Object {
+		return v[0].(float64) / v[1].(float64)
+	},
+	"^": func(v []Object) Object { // 指数
+		return math.Pow(v[0].(float64), v[1].(float64))
+	},
+	"sin": func(v []Object) Object {
+		return math.Sin(v[0].(float64))
+	},
+	"cos": func(v []Object) Object {
+		return math.Cos(v[0].(float64))
+	},
+	"tan": func(v []Object) Object {
+		return math.Tan(v[0].(float64))
+	},
+	// 其余可自行添加
+}
+
+func IsAbc(v byte) bool { // 字母判断
+	if (v >= 'a' && v <= 'z') || (v >= 'A' && v <= 'Z') {
+		return true
+	}
+	return false
+}
+
+func IsSym(v byte) bool { // 符号判断
+	syms := "@#$%^&*()-+=/?|!`~"
+	for i := 0; i < len(syms); i++ {
+		if syms[i] == v {
+			return true
+		}
+	}
+	return false
+}
+
+func IsNum(v byte) bool { // 数字判断
+	if v >= '0' && v <= '9' {
+		return true
+	}
+	return false
+}
+
+type Code struct {
+	src    string // 源码
+	pos    int    // 当前指针位置
+	tokens []Object
+}
+
+func (self *Code) Init(src string) { // 初始化
+	self.src = src
+	self.pos = 0
+	self.Next() // 初始化就读一个token
+}
+func (self *Code) Peek() Object { // 返回当前token
+	if len(self.tokens) > 0 {
+		return self.tokens[len(self.tokens)-1]
+	}
+	return nil
+}
+func (self *Code) Next() Object { // 下一个token
+	if self.pos >= len(self.src) {
+		return nil
+	}
+	var tmp []byte
+	var tk Object
+	var i = self.pos
+
+	switch {
+	case IsNum(self.src[i]) || (self.src[i] == '-' && IsNum(self.src[i+1])): // 读取数字
+		for i < len(self.src) && (IsNum(self.src[i]) || self.src[i] == '.' || self.src[i] == '-') {
+			tmp = append(tmp, self.src[i])
+			i++
+		}
+		self.pos = i
+		tk, _ = strconv.ParseFloat(string(tmp), 64)
+	case IsSym(self.src[i]): // 读取字符
+		tmp = append(tmp, self.src[i])
+		i++
+		self.pos = i
+		tk = string(tmp)
+	case IsAbc(self.src[i]): // 读取字母（变量）
+		for i < len(self.src) && IsAbc(self.src[i]) {
+			tmp = append(tmp, self.src[i])
+			i++
+		}
+		self.pos = i
+		tk = string(tmp)
+	default:
+		self.pos++
+		tk = self.Next()
+	}
+	self.tokens = append(self.tokens, tk)
+	return tk
+}
+func (self *Code) read_list() []Object { // 读列表
+	var lt []Object
+	v := self.Peek()
+	for v != ")" && v != nil {
+		if v == "(" {
+			self.Next()
+			lt = append(lt, self.read_list())
+		} else {
+			lt = append(lt, v)
+		}
+		v = self.Next()
+	}
+	return lt
+}
+
+func (self *Code) Read_Root() Object { // 根节点开始解析
+	v := self.Peek()
+	if v == "(" {
+		self.Next()
+		return self.read_list()
+	} else {
+		return v
+	}
+}
+func Apply(v []Object, fn func(Object) Object) []Object { // 将函数fn 应用到列表每一项
+	var res []Object
+	for _, j := range v {
+		res = append(res, fn(j))
+	}
+	return res
+}
+func Eval(tree Object) Object { // 计算表达式
+	switch tree.(type) {
+	case []Object:
+		v, _ := tree.([]Object)
+		if len(v) < 2 {
+			return nil
+		}
+		// 取出操作符及其对应的函数
+		f, ok := OpMap[v[0].(string)].(func([]Object) Object)
+		if ok {
+			return f(Apply(v[1:], Eval))
+		}
+	case Object:
+		return tree
+	}
+	return nil
+}
+func main() {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("User>>")
+		cmds, _, _ := reader.ReadLine()
+		cmd := string(cmds)
+		if cmd == "exit" {
+			return
+		} else {
+			c := Code{}
+			c.Init(cmd)
+			s := c.Read_Root()
+			fmt.Println("AST:", s)
+			fmt.Println(Eval(s))
+		}
+
+	}
+
+}
---
+++ 4.函数结构/main.go
@@ -0,0 +1,368 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+import (
+	"bufio"
+	"os"
+)
+
+var DEBUG = false
+
+func Debug(v ...interface{}) {
+	if DEBUG {
+		fmt.Println(v...)
+	}
+}
+
+type Object interface{}
+type List struct { // 列表，不参与运算的列表[1 2 3]
+	Val []Object
+}
+type Fn struct { // 函数结构
+	Name string             // 函数名
+	Args *List              // 形参
+	Body Object             // 函数体
+	Env  *map[string]Object // 函数环境
+}
+
+// 全局变量/函数
+var EnvMap = map[string]Object{
+	"+": func(v []Object) Object {
+		return v[0].(float64) + v[1].(float64)
+	},
+	"-": func(v []Object) Object {
+		return v[0].(float64) - v[1].(float64)
+	},
+	"*": func(v []Object) Object {
+		return v[0].(float64) * v[1].(float64)
+	},
+	"/": func(v []Object) Object {
+		return v[0].(float64) / v[1].(float64)
+	},
+	"^": func(v []Object) Object { // 指数
+		return math.Pow(v[0].(float64), v[1].(float64))
+	},
+	">": func(v []Object) Object { // 指数
+		return v[0].(float64) > v[1].(float64)
+	},
+	">=": func(v []Object) Object { // 指数
+		return v[0].(float64) >= v[1].(float64)
+	},
+
+	"<": func(v []Object) Object { // 指数
+		return v[0].(float64) < v[1].(float64)
+	},
+	"<=": func(v []Object) Object { // 指数
+		return v[0].(float64) <= v[1].(float64)
+	},
+	"==": func(v []Object) Object { // 指数
+		return v[0].(float64) == v[1].(float64)
+	},
+	"!=": func(v []Object) Object { // 指数
+		return v[0].(float64) != v[1].(float64)
+	},
+	"sin": func(v []Object) Object {
+		return math.Sin(v[0].(float64))
+	},
+	"cos": func(v []Object) Object {
+		return math.Cos(v[0].(float64))
+	},
+	"tan": func(v []Object) Object {
+		return math.Tan(v[0].(float64))
+	},
+	"print": func(v []Object) Object {
+		fmt.Println(v)
+		return nil
+	},
+	// 其余可自行添加
+}
+
+var Keywords = []string{
+	"def", // 定义函数
+	"set", // 设置参数
+}
+
+func IsAbc(v byte) bool { // 字母判断
+	if (v >= 'a' && v <= 'z') || (v >= 'A' && v <= 'Z') {
+		return true
+	}
+	return false
+}
+
+func IsSym(v byte) bool { // 符号判断
+	syms := "@#$%^&*()[]{}-+=/?|!`~<>"
+	for i := 0; i < len(syms); i++ {
+		if syms[i] == v {
+			return true
+		}
+	}
+	return false
+}
+
+func IsNum(v byte) bool { // 数字判断
+	if v >= '0' && v <= '9' {
+		return true
+	}
+	return false
+}
+func IsKeywords(word string) bool { // 判断是不是关键词
+	for _, v := range Keywords {
+		if word == v {
+			return true
+		}
+	}
+	return false
+}
+
+type Code struct {
+	src    string // 源码
+	pos    int    // 当前指针位置
+	tokens []Object
+}
+
+/**词法分析开始**/
+func (self *Code) Init(src string) { // 初始化
+	self.src = src
+	self.pos = 0
+	self.Next() // 初始化就读一个token
+}
+func (self *Code) Peek() Object { // 返回当前token
+	if len(self.tokens) > 0 {
+		return self.tokens[len(self.tokens)-1]
+	}
+	return nil
+}
+func (self *Code) Next() Object { // 下一个token
+	if self.pos >= len(self.src) {
+		return nil
+	}
+	var tmp []byte
+	var tk Object
+	var i = self.pos
+
+	switch {
+	case IsNum(self.src[i]) || (self.src[i] == '-' && IsNum(self.src[i+1])): // 读取数字
+		for i < len(self.src) && (IsNum(self.src[i]) || self.src[i] == '.' || self.src[i] == '-') {
+			tmp = append(tmp, self.src[i])
+			i++
+		}
+		self.pos = i
+		tk, _ = strconv.ParseFloat(string(tmp), 64)
+	case IsSym(self.src[i]): // 读取字符
+		if self.src[i] == '(' || self.src[i] == '[' || self.src[i] == ')' || self.src[i] == ']' {
+			tmp = append(tmp, self.src[i])
+			i++
+		} else {
+			for i < len(self.src) && IsSym(self.src[i]) {
+				tmp = append(tmp, self.src[i])
+				i++
+			}
+		}
+		self.pos = i
+		tk = string(tmp)
+	case IsAbc(self.src[i]): // 读取字母（变量）
+		for i < len(self.src) && IsAbc(self.src[i]) {
+			tmp = append(tmp, self.src[i])
+			i++
+		}
+		self.pos = i
+		tk = string(tmp)
+	default:
+		self.pos++
+		tk = self.Next()
+	}
+	self.tokens = append(self.tokens, tk)
+	return tk
+}
+func (self *Code) read_list() []Object { // 读列表
+	var lt []Object
+	v := self.Peek()
+	for v != ")" && v != nil {
+		if v == "(" {
+			self.Next()
+			lt = append(lt, self.read_list())
+		} else if v == "[" { // 不运算列表
+			var tpl List
+			v = self.Next()
+			for v != "]" && v != nil {
+				tpl.Val = append(tpl.Val, v)
+				v = self.Next()
+			}
+			lt = append(lt, tpl)
+		} else {
+			lt = append(lt, v)
+		}
+		v = self.Next()
+	}
+	return lt
+}
+
+func (self *Code) Read_Root() Object { // 根节点开始解析
+	v := self.Peek()
+	if v == "(" { // 运算列表
+		self.Next()
+		return self.read_list()
+	} else {
+		return v
+	}
+}
+
+/**词法分析结束**/
+/**计算开始**/
+func Apply(v []Object, env *map[string]Object, fn func(Object, *map[string]Object) Object) []Object { // 将函数fn 应用到列表每一项
+	var res []Object
+	for _, j := range v {
+		res = append(res, fn(Eval(j, env), env))
+	}
+	return res
+}
+func Eval(tree Object, env *map[string]Object) Object { // 计算表达式
+	/**
+	  tree token对象
+	  env 环境（变量、函数）
+	*/
+	Debug("AST:", tree)
+	switch tree.(type) {
+	case []Object:
+		v, _ := tree.([]Object)
+		// fmt.Println("switch:", len(v))
+		if len(v) < 2 {
+			return nil
+		}
+		// 取出操作符及其对应的函数
+		op := v[0].(string)
+		// fmt.Println("op:", op)
+		switch op {
+		case "set": // 设置变量值(set a 12)或者(set f (+ 1 2))
+			key := v[1].(string)
+			val := Eval(v[2], env)
+			(*env)[key] = val
+			return val
+		case "if": // 判断语句(if true 3 5)
+			du, ok := Eval(v[1], env).(bool)
+			if ok {
+				if du {
+					return Eval(v[2], env)
+				} else {
+					return Eval(v[3], env)
+				}
+			}
+		case "fn": // 函数定义 (fn fn_name [x, y] (+ x y))
+			fn_env := make(map[string]Object)
+			var fn Fn
+			for k, v := range *env { // 拷贝外环境
+				fn_env[k] = v
+			}
+			fn.Env = &fn_env
+			fn.Name = v[1].(string)
+			args := v[2].(List)
+			fn.Args = &args
+			// for _, j := range args.Val {
+			// 	fn_env[j.(string)] = nil
+			// }
+			fn.Body = v[3]
+			(*env)[fn.Name] = fn    // 向上一层环境中加入函数
+			(*fn.Env)[fn.Name] = fn // 要想实现递归,就应当在自己的环境中找到自己,这是必须的
+			return fn
+		default:
+			if f, ok := (*env)[op]; ok {
+				switch f.(type) {
+				case func([]Object) Object: // 系统函数
+					fc := f.(func([]Object) Object)
+					return fc(Apply(v[1:], env, Eval))
+				case Fn: // 自定义函数 (fn_name args1 args2 ...)
+					// fmt.Println("自定义函数:", op)
+					fc := f.(Fn)
+					fenv := make(map[string]Object) // 环境拷贝
+					for k, v := range *(fc.Env) {
+						fenv[k] = v
+					}
+					// 取传入函数的参数(可能是表达式)
+					args := v[1:]
+					for i, j := range args { // 将传递的参数加入函数环境
+						fenv[fc.Args.Val[i].(string)] = Eval(j, env)
+					}
+					return Eval(fc.Body, &fenv)
+				}
+
+			}
+		}
+
+	case Object:
+		switch tree.(type) {
+		case string:
+			if k, ok := (*env)[tree.(string)]; ok {
+				//存在变量
+				return k
+			}
+		}
+		return tree
+	}
+	return nil
+}
+
+/*计算结束*/
+func main() {
+
+	/**
+		// 斐波拉契递归测试
+		c := Code{}
+		c.Init("(fn f [x] (if (<= x 2) 1 (+ (f (- x 1)) (f (- x 2)))))")
+		s := c.Read_Root()
+		fmt.Println(s)
+		fmt.Println(Eval(s, &EnvMap))
+		c1 := Code{}
+		c1.Init("(f 1)")
+		fmt.Println(Eval(c1.Read_Root(), &EnvMap))
+		c2 := Code{}
+		c2.Init("(f 2)")
+		fmt.Println(Eval(c2.Read_Root(), &EnvMap))
+		c3 := Code{}
+		c3.Init("(f 3)")
+		fmt.Println(Eval(c3.Read_Root(), &EnvMap))
+		c4 := Code{}
+		c4.Init("(f 4)")
+		fmt.Println(Eval(c4.Read_Root(), &EnvMap))
+		c5 := Code{}
+		c5.Init("(f 5)")
+		fmt.Println(Eval(c5.Read_Root(), &EnvMap))
+		c6 := Code{}
+		c6.Init("(f 6)")
+		fmt.Println(Eval(c6.Read_Root(), &EnvMap))
+	    **/
+	/*
+		// 函数相互调用测试
+		c := Code{}
+		c.Init("(fn f [x] (+ x x))")
+		s := c.Read_Root()
+		fmt.Println(s)
+		fmt.Println(Eval(s, &EnvMap))
+		c2 := Code{}
+		c2.Init("(fn g [x] (* x (f x)))")
+		fmt.Println(Eval(c2.Read_Root(), &EnvMap))
+		c3 := Code{}
+		c3.Init("(g 2)")
+		fmt.Println(Eval(c3.Read_Root(), &EnvMap))
+	*/
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("User>>")
+		cmds, _, _ := reader.ReadLine()
+		cmd := string(cmds)
+		if cmd == "exit" {
+			return
+		} else {
+			c := Code{}
+			c.Init(cmd)
+			s := c.Read_Root()
+			fmt.Println(Eval(s, &EnvMap))
+		}
+
+	}
+
+}
